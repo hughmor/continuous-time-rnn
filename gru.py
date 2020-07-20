363644b9fffe6abbe6a4d85af1c8f81fcdcb0640
Hugh Morison <hugh.morison@hotmail.ca>
2020-07-20 09:16:09 -0400

    created child classes

---
+++ gru.py
@@ -0,0 +1,6 @@
+from rnn import np, deque, CTRNN
+
+class CTGRU(CTRNN):
+    def __init__(self):
+        pass
+
---
+++ lstm.py
@@ -0,0 +1,6 @@
+from rnn import np, deque, CTRNN
+
+class CTLSTM(CTRNN):
+    def __init__(self):
+        pass
+
