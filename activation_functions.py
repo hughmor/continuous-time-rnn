1aecba35a6f02045d3699704280d606b76fecd3c
Hugh Morison <36174401+hughmor@users.noreply.github.com>
2021-04-09 23:46:02 -0400

    added a number of activation functions

--- activation_functions.py
+++ activation_functions.py
@@ -5,8 +5,11 @@
     This file contains some common activation functions for use in ANNs.
     All functions operate on an input $x$ (scalar, list, or numpy array) and return the output in a numpy array.
 
+    TODO: add input magnitude contraints? most functions are periodic or saturating...
+    TODO: add log, exp -> these need reasonable bounds
 """
 import numpy as np
+
 
 def linear(x):
     """
@@ -18,6 +21,7 @@ def linear(x):
     x = np.atleast_1d(x)
     return x
 
+
 def relu(x):
     """
     Rectified Linear Unit Function.
@@ -27,6 +31,17 @@ def relu(x):
     """
     x = np.atleast_1d(x)
     return np.maximum(np.zeros_like(x), x)
+
+
+def elu(x):
+    """
+    Exponential Linear Unit Function.
+
+    :param x: input vector
+    :return: output of min(max(0,x),exp(x)-1)
+    """
+    x = np.atleast_1d(x)
+    return np.minimum(np.maximum(np.zeros_like(x), x), np.exp(x) - 1)
 
 
 def softplus(x):
@@ -48,7 +63,7 @@ def sigmoid(x):
     :return: output of 1/(1+exp(-x))
     """
     x = np.atleast_1d(x)
-    return 1/(1+np.exp(-x))
+    return 1/(1+np.exp(-1*x))
 
 
 def tanh(x):
@@ -60,3 +75,71 @@ def tanh(x):
     """
     x = np.atleast_1d(x)
     return np.tanh(x)
+
+
+def sin(x):
+    """
+    Sinusoidal Function.
+
+    :param x: input vector
+    :return: output of sin(x)
+    """
+    x = np.atleast_1d(x)
+    return np.sin(x)
+
+
+def clamp(x):
+    """
+    Clamped Function.
+
+    :param x: input vector
+    :return: output of max(-1, min(1, x))
+    """
+    x = np.atleast_1d(x)
+    one = np.ones_like(x)
+    return np.maximum(-1*one, np.minimum(one, x))
+
+
+def square(x):
+    """
+    Square Function.
+
+    :param x: input vector
+    :return: output of x*x
+    """
+    x = np.atleast_1d(x)
+    return x*x
+
+
+def cube(x):
+    """
+    Cube Function.
+
+    :param x: input vector
+    :return: output of x*x*x
+    """
+    x = np.atleast_1d(x)
+    return x*x*x
+
+
+def absolute(x):
+    """
+    Absolute Value Function.
+
+    :param x: input vector
+    :return: output of |x|
+    """
+    x = np.atleast_1d(x)
+    return np.abs(x)
+
+
+def hat(x):
+    """
+    Hat Function.
+
+    :param x: input vector
+    :return: output of max(0, 1-|x|)
+    """
+    x = np.atleast_1d(x)
+    return np.maximum(np.zeros_like(x), 1-np.abs(x))
+
